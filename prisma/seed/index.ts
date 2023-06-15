c6aa3b42e98585c3b1ad6232fe59d0dfc935637c
Oleksandr Petryk <oleksandr.petryk@incode-it.com>
2023-06-15 20:02:27 +0300

    chore: update prisma schema and setup seed

---
+++ prisma/seed/index.ts
@@ -0,0 +1,42 @@
+import { PrismaClient } from '@prisma/client';
+import * as fs from 'fs/promises';
+import * as path from 'path';
+
+const client = new PrismaClient();
+
+async function checkEnglishLanguageExistance() {
+  const englishLanguage = await client.language.findMany({
+    where: {
+      code: 'en',
+    },
+  });
+
+  if (englishLanguage.length === 0) {
+    const englishLanguageIcon = await fs.readFile(
+      path.join(
+        path.resolve(),
+        'prisma',
+        'seed',
+        'data',
+        'english-language-icon.jpeg',
+      ),
+    );
+
+    await client.language.create({
+      data: {
+        code: 'en',
+        icon: {
+          create: {
+            icon: englishLanguageIcon,
+          },
+        },
+      },
+    });
+  }
+}
+
+async function index() {
+  await checkEnglishLanguageExistance();
+}
+
+index();
