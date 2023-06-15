bf88040ee9a3d614a882a8dda9b95abd4551e7c6
Oleksandr Petryk <oleksandr.petryk@incode-it.com>
2023-06-15 16:41:48 +0300

    chore: configure server and define prisma module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,22 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 
 import { LanguagesController } from './languages/languages.controller';
 import { LanguagesModule } from './languages/languages.module';
 import { LanguagesService } from './languages/services/languages.service';
+import { PrismaModule } from './shared/prisma/prisma.module';
+import { TranslationModule } from './translation/translation.module';
 
 @Module({
-  imports: [LanguagesModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: ['../.env'],
+    }),
+    PrismaModule,
+    LanguagesModule,
+    TranslationModule,
+  ],
   controllers: [LanguagesController],
   providers: [LanguagesService],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,44 @@
+import { ValidationPipe } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import * as cookieParser from 'cookie-parser';
 
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  // Setup middleware
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      whitelist: true,
+    }),
+  );
+  app.enableCors();
+  app.use(cookieParser());
+
+  // Setup Swagger
+  const config = new DocumentBuilder()
+    .setTitle('Translation')
+    .setDescription('Translation API documentation')
+    .setVersion('0.0.1')
+    .addSecurity('Bearer', {
+      type: 'http',
+      scheme: 'Bearer',
+    })
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  const configService = app.get(ConfigService);
+  const PORT = configService.get('PORT');
+
+  // Run server
+  await app.listen(PORT, () => {
+    console.log(`Server started on http://localhost:${PORT}`);
+  });
 }
 bootstrap();
