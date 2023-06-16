921d3a86e4bd3b285abed62b63d64ff64a2851fc
Oleksandr Petryk <oleksandr.petryk@incode-it.com>
2023-06-16 12:12:21 +0300

    chore: add few other languages to the seed

--- prisma/seed/index.ts
+++ prisma/seed/index.ts
@@ -4,34 +4,45 @@ import * as path from 'path';
 
 const client = new PrismaClient();
 
+const languageIconsPath = path.join(path.resolve(), 'prisma', 'seed', 'data');
+
+const languagesList = [
+  {
+    code: 'en',
+    iconPath: path.join(languageIconsPath, 'english-language-icon.jpeg'),
+  },
+  {
+    code: 'ua',
+    iconPath: path.join(languageIconsPath, 'ukraine-language-icon.png'),
+  },
+  {
+    code: 'ja',
+    iconPath: path.join(languageIconsPath, 'japan-language-icon.png'),
+  },
+];
+
 async function checkEnglishLanguageExistance() {
-  const englishLanguage = await client.language.findMany({
-    where: {
-      code: 'en',
-    },
-  });
-
-  if (englishLanguage.length === 0) {
-    const englishLanguageIcon = await fs.readFile(
-      path.join(
-        path.resolve(),
-        'prisma',
-        'seed',
-        'data',
-        'english-language-icon.jpeg',
-      ),
-    );
-
-    await client.language.create({
-      data: {
-        code: 'en',
-        icon: {
-          create: {
-            icon: englishLanguageIcon,
-          },
-        },
+  for (const languageItem of languagesList) {
+    const check = await client.language.findFirst({
+      where: {
+        code: languageItem.code,
       },
     });
+
+    if (!check) {
+      const languageIcon = await fs.readFile(languageItem.iconPath);
+
+      await client.language.create({
+        data: {
+          code: languageItem.code,
+          icon: {
+            create: {
+              icon: languageIcon,
+            },
+          },
+        },
+      });
+    }
   }
 }
 
